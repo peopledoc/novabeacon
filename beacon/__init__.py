1e7c589d123b1e139deb42f2cec4b5d475004204
FELD Boris <lothiraldan@gmail.com>
2013-04-11 14:16:15 +0200

    Add first version of beacon module

---
+++ beacon/__init__.py
@@ -0,0 +1,18 @@
+import json
+
+class Beacon(object):
+
+    def __init__(self, redisclient):
+        self.client = redisclient
+
+    def publish(self, channel, dict_msg):
+        self.client.publish(channel, json.dumps(dict_msg))
+
+    def blue(self, dict_msg):
+        self.publish('blue_beacon', dict_msg)
+
+    def red(self, dict_msg):
+        self.publish('red_beacon', dict_msg)
+
+    def yellow(self, dict_msg):
+        self.publish('yellow_beacon', dict_msg)
