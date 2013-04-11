86d4669a491b248c9ec0aaf89b43e953e02a0473
FELD Boris <lothiraldan@gmail.com>
2013-04-11 14:57:26 +0200

    Add default value for dict_msg

--- beacon/__init__.py
+++ beacon/__init__.py
@@ -8,11 +8,11 @@ class Beacon(object):
     def publish(self, channel, dict_msg):
         self.client.publish(channel, json.dumps(dict_msg))
 
-    def blue(self, dict_msg):
+    def blue(self, dict_msg={}):
         self.publish('blue_beacon', dict_msg)
 
-    def red(self, dict_msg):
+    def red(self, dict_msg={}):
         self.publish('red_beacon', dict_msg)
 
-    def yellow(self, dict_msg):
+    def yellow(self, dict_msg={}):
         self.publish('yellow_beacon', dict_msg)
